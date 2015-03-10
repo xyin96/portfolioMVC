d735de4543964cf0eb77e95fa321d08fc2651120
xyin96 <yin530@gmail.com>
2015-03-10 14:44:28 -0400

    reactive background to local weather

--- model/views/Background.php
+++ model/views/Background.php
@@ -8,23 +8,40 @@
 
 class Background {
     public function __construct(){
-        $city="College Park";
-        $country="US"; //Two digit country code
-        $url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
-        $json=file_get_contents($url);
-        $data=json_decode($json,true);
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+        $loc=explode(',',$details->loc);
+        $country=$details->country; //Two digit country code
+        $url="http://api.openweathermap.org/data/2.5/weather?lat=".$loc[0]."&lon=".$loc[1]."&cnt=7&lang=en";
+        $weather=file_get_contents($url);
+        $data=json_decode($weather,true);
         //Get cloud percentage $multi * 0x0E0E0E
-        $clouds = intval($data['clouds']['all']) / 20;
+        $clouds = intval($data['clouds']['all']) / 10;
 
+        print '<script>$(function(){console.log("lat='.$loc[0].'&lon='.$loc[1].'"); console.log("country: '. $country .'"); console.log("weather: '. $data['weather'][0]['main'] .'"); console.log("cloud coverage: ' . $data['clouds']['all'] . '")});</script>';
         $time = intval(date("H"));
         $multi = abs($time - 12);
-        $hex = dechex(0xc9dbe9);
+        $hex = dechex($this->getBG($data['weather'][0]['main']));
         print '<link rel="stylesheet" href="public/css/background.css" />';
-        print '<style>#clouds{    background: #'.$hex.';   background: -webkit-linear-gradient(top, #'.$hex.' 0%, #fff 100%);  background: -linear-gradient(top, #'.$hex.' 0%, #fff 100%);  background: -moz-linear-gradient(top, #c9dbe9 0%, #fff 100%);}</style>';
-        print '<div id="clouds" class="background"> 	<div class="cloud x1"></div> 	<!-- Time for multiple clouds to dance around --> 	<div class="cloud x2"></div> 	<div class="cloud x3"></div> 	<div class="cloud x4"></div> 	<div class="cloud x5"></div>';
+        print '<style>#clouds{    background: #'.$hex.';   background: -webkit-linear-gradient(top, #'.$hex.' 0%, #fff 100%);  background: -linear-gradient(top, #'.$hex.' 0%, #fff 100%);  background: -moz-linear-gradient(top, #'.$hex.' 0%, #fff 100%);}</style>';
+        print '<div id="clouds" class="background"> ';
         for(;$clouds > 1; --$clouds){
-            print'<div class="cloud x'. (5 + intval($clouds)).'"></div>';
+            print'<div class="cloud x'. (intval($clouds)).'"></div>';
         }
         print '</div>';
+    }
+
+    protected function getBG($data){
+        switch($data){
+            case "Clouds":
+                return $hex = 0xAAAAAA;
+                break;
+            case "Clear":
+                return $hex = 0xc9dbe9;
+            break;
+            default:
+                return $hex = 0xc9dbe9;
+            break;
+        }
     }
 }
